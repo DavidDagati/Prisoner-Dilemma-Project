195e4ea16348b007755e85ec5f0acd0f3a21b56c
David Dagati <ddagati@hotmail.ca>
2023-02-02 23:04:56 -0500

    Pavlov V2.0

--- Strategies/pavlov.py
+++ Strategies/pavlov.py
@@ -5,29 +5,20 @@ def pavlov(logFile, playerID):
 
     # Read existing log
     with open(logFile, 'r') as log:
-        last_line = log.readlines()[-1]
-
-    if (last_line is None):
-        # Start with cooperate
-        result = 0
-    else:
-        # With at least one row of data Pavlov occurs
-        if (last_line[0] == last_line[2]):
-            result = 0
-        else:
-            result = 1
+        try:
+            last_line = log.readlines()[-1]
+        except IndexError:
+            last_line = None
 
     # Close log for reading
     log.close()
 
-    # Open the logFile for appending
-    log = open(logFile, 'a')
-
-    # Write the result in logFile
-    if playerID == "A":
-        log.write("\n" + result)
-    elif playerID == "B":
-        log.write(" " + result)
-
-    # Close log for appending
-    log.close()
+    if (last_line is None):
+        # Start with cooperate
+        return 0
+    else:
+        # With at least one row of data Pavlov occurs
+        if (last_line[0] == last_line[2]):
+            return 0
+        else:
+            return 1
