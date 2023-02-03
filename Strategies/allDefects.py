995fc8c4eff6a411b6725f4c7cf5c54615921ebd
David Dagati <ddagati@hotmail.ca>
2023-02-02 22:43:23 -0500

    All Defects V2.0

--- Strategies/allDefects.py
+++ Strategies/allDefects.py
@@ -6,14 +6,4 @@ def allDefects(logFile, playerID):
     # Set Result to Defect
     result = 1
 
-    # Open the logFile for appending
-    log = open(logFile, 'a')
-
-    # Write the result in logFile
-    if playerID == "A":
-        log.write("\n" + result)
-    elif playerID == "B":
-        log.write(" " + result)
-
-    # Close log for appending
-    log.close()
+    return result
