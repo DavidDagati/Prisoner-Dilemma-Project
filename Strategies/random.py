7f018367ee3a377518d3e12d90e5dc82fc823d59
David Dagati <ddagati@hotmail.ca>
2023-02-13 12:29:00 -0500

    Rename random file because module is weird

--- Strategies/random.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def Random(logFile, playerID):
-    return random.randint(0,1)
-
-# Test:
-# print(Random('../Results/log.txt', 1))
---
+++ Strategies/random_strat.py
@@ -0,0 +1,7 @@
+import random as rand
+
+def random(logFile, playerID):
+    return rand.randint(0,1)
+
+# Test:
+# print(Random('../Results/log.txt', 1))
